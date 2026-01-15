33b94ea4b4c251851261c938438efe15c2ea0cfc
Deadboys Corner <48094316+Deadboy666@users.noreply.github.com>
2026-01-15 00:27:29 -0600

    force stable client for sanity check

--- headcrab.sh
+++ headcrab.sh
@@ -62,9 +62,9 @@ set -eu
         killall wheresteam || true
         echo "the headcrab approaches.."
         echo "the headcrab lactches on the steam process.."
-        export_sls wheresteam steam://exit &> /dev/null
+        export_sls wheresteam -clearbeta steam://exit &> /dev/null
     else
-        export_sls wheresteam steam://exit &> /dev/null
+        export_sls wheresteam -clearbeta steam://exit &> /dev/null
     fi
         conditioncheck
         }
