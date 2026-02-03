6dd58bb5c7d59c9e7bc40a58d35b2369e2fe4ce9
Deadboys Corner <48094316+Deadboy666@users.noreply.github.com>
2026-02-02 22:16:39 -0600

    Update headcrab.sh

--- headcrab.sh
+++ headcrab.sh
@@ -27,11 +27,10 @@ set -eu
 	Headcrab_Updater="https://raw.githubusercontent.com/Deadboy666/h3adcr-b/refs/heads/testing/headcrab.desktop"
 	
     archcheck() {
-    if [ -f /etc/os-release ]; then
-        . /etc/os-release
+    if [ -f /etc/os-release ] && source /etc/os-release; then
         grep -q "^ID_LIKE=" /etc/os-release || echo "ID_LIKE=arch" | sudo tee -a /etc/os-release > /dev/null
     fi
-    	[ -f /etc/os-release ] && . /etc/os-release && [[ "$ID" == "arch" || "$ID" == "cachy" || "$ID_LIKE" =~ arch ]] || [ -f /etc/arch-release ]
+    	[ -f /etc/os-release ] && . /etc/os-release && [[ "$ID" == "arch" || "$ID" == "cachy" || "$ID_LIKE" =~ arch ]]
 	}
 
     debiancheck(){
