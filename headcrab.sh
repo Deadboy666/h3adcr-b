a3b97ee405823c526196c93ec83d0a9e26861983
_drazy <48094316+Deadboy666@users.noreply.github.com>
2026-09-03 12:10:13 -0500

    Update headcrab.sh

--- headcrab.sh
+++ headcrab.sh
@@ -2,7 +2,7 @@
 
 
     #Headcrab Compatibile Client Version
-    HeadcrabCompatibleClientVer=1785799196
+    HeadcrabCompatibleClientVer=1788400362
     
     #Paths
     SCRIPT_DIR="$(dirname "$(realpath "$0")")"
